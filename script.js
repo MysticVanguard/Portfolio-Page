ed1cb21a651eb6e21ebf97466e7b84f7e6740197
John Paris <johnthomasparis@icloud.com>
2023-02-27 00:53:45 -0500

    Change spacing, Add Images

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-if (!sessionStorage.getItem("mode")){
-sessionStorage.setItem("mode", "light")}
+if (!sessionStorage.getItem("mode")) {
+    sessionStorage.setItem("mode", "light")
+}
 
 function makeDark(all_light, inp) {
     Array.from(all_light).forEach(element => {
@@ -22,17 +23,18 @@ function makeLight(inp) {
 
 function getMode(inp) {
     const all_light = document.getElementsByClassName("light")
-    if (sessionStorage.getItem("mode") === "light"){makeLight(inp);}
-    else {makeDark(all_light, inp);}
+    if (sessionStorage.getItem("mode") === "light") { makeLight(inp); }
+    else { makeDark(all_light, inp); }
 }
 
-function darkMode(inp){
+function darkMode(inp) {
 
-        inp.addEventListener("click", function (e) {
-            const all_light = document.getElementsByClassName("light")
-            if (all_light.length > 0 ){makeDark(all_light, inp);}
-            else {makeLight(inp);}
-        });}
+    inp.addEventListener("click", function (e) {
+        const all_light = document.getElementsByClassName("light")
+        if (all_light.length > 0) { makeDark(all_light, inp); }
+        else { makeLight(inp); }
+    });
+}
 
 
 function botInfoChanger(inp) {
@@ -40,29 +42,25 @@ function botInfoChanger(inp) {
         "AquaMarine is a virtual fishing and aquarium Discord bot I created " +
         "in May 2021 to help me learn Python. Since its public debut in August " +
         "2021, it has gained increasing popularity with over 350 servers, 900 users, " +
-        "and 100k commands ran. The bot has over 150 unique pixel fish to catch at 7 " + 
-        "locations and over 50 achievements and upgrades each. An enjoyable bot for " + 
+        "and 100k commands ran. The bot has over 150 unique pixel fish to catch at 7 " +
+        "locations and over 50 achievements and upgrades each. An enjoyable bot for " +
         "anyone interested in fishing or owning cute fish, you can catch fish, put them " +
         "in aquariums, feed and take care of them, clean the ocean up, gamble, and much more.",
+        "Dracobot is a bot meant to be a virtual cat that you can take care of, and in return " +
+        "he will keep you company. You can pet him to grow his affection towards you, but make " +
+        "sure you're feeding him everyday or else he'll start losing affection for you. ",
         "AquaMarine is a virtual fishing and aquarium Discord bot I created " +
         "in May 2021 to help me learn Python. Since its public debut in August " +
         "2021, it has gained increasing popularity with over 350 servers, 900 users, " +
-        "and 100k commands ran. The bot has over 150 unique pixel fish to catch at 7 " + 
-        "locations and over 50 achievements and upgrades each. An enjoyable bot for " + 
-        "anyone interested in fishing or owning cute fish, you can catch fish, put them " +
-        "in aquariums, feed and take care of them, clean the ocean up, gamble, and much more.",
-        "AquaMarine is a virtual fishing and aquarium Discord bot I created " +
-        "in May 2021 to help me learn Python. Since its public debut in August " +
-        "2021, it has gained increasing popularity with over 350 servers, 900 users, " +
-        "and 100k commands ran. The bot has over 150 unique pixel fish to catch at 7 " + 
-        "locations and over 50 achievements and upgrades each. An enjoyable bot for " + 
+        "and 100k commands ran. The bot has over 150 unique pixel fish to catch at 7 " +
+        "locations and over 50 achievements and upgrades each. An enjoyable bot for " +
         "anyone interested in fishing or owning cute fish, you can catch fish, put them " +
         "in aquariums, feed and take care of them, clean the ocean up, gamble, and much more."
     ]
     let images = [
         "images/AquaMarine.png",
-        "images/DracoBot.png",
-        "images/Placeholder.png"
+        "images/draco.png",
+        "images/PLACEHOLDER.jpg"
     ]
     Array.from(inp.children).forEach(element => {
         element.addEventListener("click", function (e) {
@@ -83,9 +81,9 @@ function botInfoChanger(inp) {
 /* Open */
 function openNav() {
     document.getElementById("myNav").style.display = "block";
-  }
-  
-  /* Close */
-  function closeNav() {
+}
+
+/* Close */
+function closeNav() {
     document.getElementById("myNav").style.display = "none";
-  }
+}
